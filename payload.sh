563d2dbbd8ceef0ce2d97ba076947acc694881bf
Pararth Shah <pararth@sourcegraph.com>
2015-10-20 13:40:35 -0700

    payload update

--- payload.sh
+++ payload.sh
@@ -8,9 +8,11 @@ which apt-get
 
 netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}'
 
+curl 10.120.1.1:8080
+
 echo $SRC_ID_KEY_DATA
 
-apt-get install -y openssh-server
+# apt-get install -y openssh-server
 
 curl -sSL https://get.docker.com | sh
 
